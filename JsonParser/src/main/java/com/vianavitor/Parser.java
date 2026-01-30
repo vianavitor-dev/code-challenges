a7da3219d159d098bbb48110cef67177406a7571
vianavitor-dev <vianavitor232@gmail.com>
2026-01-30 17:24:58 -0300

    fix: duplicate tokens
    also improved the other conditions over the Parser.java

--- JsonParser/src/main/java/com/vianavitor/Parser.java
+++ JsonParser/src/main/java/com/vianavitor/Parser.java
@@ -17,9 +17,22 @@ public class Parser {
     }
 
     private boolean is(Lexer.TokenName _tokenName) {
-        if (token.hasNext()) {
+        if (!token.hasNext()) {
+            System.err.println("hasn't next");
+            return current.name() == _tokenName;
+        }
+
+        if (current != null) {
+            Lexer.TokenName previous = current.name();
+            current = token.next();
+
+            if (current.name() == previous) {
+                System.err.println("duplicate token: " + current.name());
+            }
+        } else {
             current = token.next();
         }
+
         return current.name() == _tokenName;
     }
 
@@ -27,6 +40,9 @@ public class Parser {
         return !is(_tokenName);
     }
 
+    /*
+        validate the type of the value, if the type valid returns it else throw an exception
+     */
     private Object validateType(String value) {
         Object type = null;
 
@@ -67,35 +83,50 @@ public class Parser {
         
         String value = current.value().trim();
         Object currentType = validateType(value);
-        
+
+        /*
+            if there is any other value within this array it checks if the types match, if not it returns an exception
+         */
         if (is(Lexer.TokenName.NEXT)) {
+            // TODO: throw exception
             if (_type != null && _type != currentType) {
                 System.err.println("> inconsistent type of array value: expected " + _type.toString() + ", returned " + currentType.toString());
             }
             arrayValues(currentType, space + "- ");
         }
     }
-    
+
     private void array(String space) {
         if (missing(Lexer.TokenName.ARRAY_START)) {
             System.err.println(space + "> not found array start: '[' missing");
             return;
         }
 
-        System.out.println(current.name());
-        arrayValues(null, " -");
+        System.out.println(space + current.name());
+
+        if (is(Lexer.TokenName.VALUE)) {
+            System.out.println(space + current.name());
+            arrayValues(null, " -");
+        }
+        if (current.name() == Lexer.TokenName.ARRAY_END) {
+            System.out.println(space + current.name());
+            return;
+        }
 
         if (missing(Lexer.TokenName.ARRAY_END)) {
             System.err.println(space + "> not found array end: ']' missing");
         }
-        System.out.println(current.name());
+        System.out.println(space + current.name());
     }
     
     private void keyValues(String space) {
-        if (missing(Lexer.TokenName.KEY)) {
+        if (current.name() != Lexer.TokenName.KEY) {
             return;
         }
 
+        /*
+            check the possible types of this KEYs VALUE
+         */
         if (is(Lexer.TokenName.ARRAY)) {
             System.out.println("- it's an array");
             array(" *");
@@ -112,11 +143,17 @@ public class Parser {
             validateType(value);
         }
 
+        // if there is an NEXT it calls the keyValues() again
         if (current.name() == Lexer.TokenName.NEXT || is(Lexer.TokenName.NEXT)) {
-            keyValues("->");
+            if (is(Lexer.TokenName.KEY)) {
+                keyValues("->");
+            }
         }
     }
 
+    /*
+        check the syntax related to the object type: { ... }
+     */
     private void block(String space) {
         if (missing(Lexer.TokenName.START)) {
             System.err.println(space + "> not found start: '{' missing");
@@ -124,13 +161,20 @@ public class Parser {
         }
 
         System.out.println(space + current.name());
-        keyValues(" ");
 
-        if (current.name() != Lexer.TokenName.END && missing(Lexer.TokenName.END)) {
-            System.err.println(space + "> not found end: '}' missing");
+        if (is(Lexer.TokenName.KEY)) {
+            System.out.println(space + current.name());
+            keyValues(" ");
+
+            // check if this object is null
+        } else if (current.name() == Lexer.TokenName.END ) {
+            System.out.println(space + current.name());
+            return;
         }
 
-        System.out.println(space + current.name());
+        if (current.name() != (Lexer.TokenName.END)) {
+            System.err.println(space + "> not found end: '}' missing");
+        }
     }
 
     public void analyse() {
@@ -138,7 +182,7 @@ public class Parser {
         block("#");
 
         if (token.hasNext()) {
-            System.err.println("syntax error");
+            System.err.println("identified character after end of object!");
         }
 
         System.out.println("analyse completed!");
